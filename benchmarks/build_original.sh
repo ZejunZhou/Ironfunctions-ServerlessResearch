f27a54fb2f2578875853da90000d009b5d4db30c
Ad-Bean <adbeanx@outlook.com>
2024-12-09 21:22:20 -0500

    chore: test script

--- benchmarks/build_original.sh
+++ benchmarks/build_original.sh
@@ -5,12 +5,12 @@ cd ./original/hotel_user
 fn init --runtime go original_user
 fn build
 fn routes create app /user
-fn routes update app /user -max-concurrency 8 -m 256 --timeout 60s
+fn routes update app /user
 cd ../..
 
 cd ./original/hotel_hotels
 fn init --runtime go original_hotels
 fn build
 fn routes create app /hotels
-fn routes update app /hotels -max-concurrency 8 -m 256 --timeout 60s
+fn routes update app /hotels
 cd ..
