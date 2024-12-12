7460df86b65722a0ff56ff5b97fddc0d1849ee7d
Ad-Bean <adbeanx@outlook.com>
2024-12-11 22:49:08 -0500

    scripts

--- benchmarks/build.sh
+++ benchmarks/build.sh
@@ -3,7 +3,7 @@
 cd ./hotel_hotels
 fn init --runtime go hotels
 fn build
-fn routes create hotel /hotels
+fn routes create hotel /hotels -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 fn routes update hotel /hotels -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 cd ..
 
@@ -11,14 +11,14 @@ cd ..
 cd ./hotel_user
 fn init --runtime go user
 fn build
-fn routes create hotel /user
+fn routes create hotel /user -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 fn routes update hotel /user -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 cd ..
 
 cd ./hotel_recommendations
 fn init --runtime go recommendations
 fn build
-fn routes create hotel /recommendations
+fn routes create hotel /recommendations  -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 fn routes update hotel /recommendations -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 cd ..
 
@@ -27,4 +27,9 @@ fn init --runtime go reservation
 fn build
 fn routes create hotel /reservation
 fn routes update hotel /reservation -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
+cd ..
+
+cd ./hotel_rate
+fn build
+fn routes create hotel /rate -max-concurrency 8 -m 512 --format http --timeout 60s --idle-timeout 600s
 cd ..
--- benchmarks/scripts/docker/mongodb.sh
+++ benchmarks/scripts/docker/mongodb.sh
@@ -1,1 +1,3 @@
 docker run -d --name mongodb-hotelresv --hostname socialnetwork-mongodb -p 27017:27017 -v /mnt/inmem/db:/data/db mongo:4.2.8-bionic mongod --nojournal
+
+docker run -d --name memcached-reserve --hostname memcached-reserve --restart always -m 4096m -p 11211:11211 memcached:1.5.22 memcached -m 4096
